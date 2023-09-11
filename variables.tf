39e444475b6bbd2604af56c3a36219ca41f1ad8e
kaizenacademy <kaizenacademy@gmail.com>
2023-09-11 21:12:39 +0000

    new files

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable ami_id {
+  type        = string
+  default     = ""
+  description = "Proved AMI id"
+}
+
+variable instance_type {
+  type        = string
+  default     = ""
+  description = "Proved Instance type"
+}
+
+variable az1 {
+  type        = string
+  default     = ""
+  description = "Proved availability zone"
+}
+
+variable key_pair {
+  type        = string
+  default     = ""
+  description = "Proved key pair name"
+}
+
+
